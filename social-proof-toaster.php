40cc96a48b4095350c7db2d7e86955b647909e58
haonx <qtvhao@gmail.com>
2018-03-11 12:13:07 +0700

    refactoring

--- social-proof-toaster.php
+++ social-proof-toaster.php
@@ -665,9 +665,54 @@ function sproof_inject_footer_start_html() {
 	
 }
 CSS;
-	echo '<div id="WpBx2DbIonOp2DbIVcYt_0"><div id="WpBx3tRBonOp3tRBVcYt_0" class=""><div><div style=""><div style=""><div id="WpBx2fZIonOp2fZIVcYt_0"><div class="WpBx3zjZonOp3zjZVcYt_0 yp_onscreen"><div class="WpBx1JYFonOp1JYFVcYt_0"><img class="" src="https://firebasestorage.googleapis.com/v0/b/proof-f6589.appspot.com/o/maps%2Fc2ca8c9de6e93ee0d2348d586f1df432.png.png?alt=media"></div> <div class="WpBx2EbConOp2EbCVcYt_0"><div class="WpBx2AUlonOp2AUlVcYt_0">Arun from New Delhi, DL</div> <div class="WpBx13BWonOp13BWVcYt_0">Recently signed up for Proof</div> <div class="WpBx2PlWonOp2PlWVcYt_0">2 hours ago</div> <div class="WpBx3eNuonOp3eNuVcYt_0"><!----> <i><svg width="7" height="13" viewBox="0 0 7 13" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M4.127.496C4.51-.12 5.37.356 5.16 1.07L3.89 5.14H6.22c.483 0 .757.616.464 1.044l-4.338 6.34c-.407.595-1.244.082-1.01-.618L2.72 7.656H.778c-.47 0-.748-.59-.48-1.02L4.13.495z" fill="#F6A623"></path><path fill="#FEF79E" d="M4.606.867L.778 7.007h2.807l-1.7 5.126 4.337-6.34H3.16"></path></g></svg></i> <span>by</span> <span class="WpBx12wMonOp12wMVcYt_0">Proof</span></div></div> <div class="WpBx1N_EonOp1N_EVcYt_0" style="display: none;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path></svg></div></div></div></div> <div style="display: none;"><div id="WpBx3dGionOp3dGiVcYt_0"><div class="WpBx2B_4onOp2B_4VcYt_0"><div id="WpBx3acHonOp3acHVcYt_0"><div class="WpBxH0WDonOpH0WDVcYt_0"><div class="WpBxh0ePonOph0ePVcYt_0"></div></div> <div class="WpBx15RvonOp15RvVcYt_0"><div class="WpBx2fwXonOp2fwXVcYt_0"><span class="WpBx1Jr6onOp1Jr6VcYt_0"><span>113</span></span> people</div> are viewing this site
-      </div> <div class="WpBx14UVonOp14UVVcYt_0">
-        Verified by <i><svg width="7" height="13" viewBox="0 0 7 13" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M4.127.496C4.51-.12 5.37.356 5.16 1.07L3.89 5.14H6.22c.483 0 .757.616.464 1.044l-4.338 6.34c-.407.595-1.244.082-1.01-.618L2.72 7.656H.778c-.47 0-.748-.59-.48-1.02L4.13.495z" fill="#F6A623"></path><path fill="#FEF79E" d="M4.606.867L.778 7.007h2.807l-1.7 5.126 4.337-6.34H3.16"></path></g></svg></i> <a href="https://useproof.com/?utm_campaign=P3R&amp;utm_content=LVC" rel="nofollow" target="_blank">Proof</a></div></div> <div class="WpBx159fonOp159fVcYt_0" style="display: none;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path></svg></div></div></div></div> <div style="display: none;"><div id="WpBx2lrionOp2lriVcYt_0"><div class="WpBx1XogonOp1XogVcYt_0"><div class="WpBx37xponOp37xpVcYt_0"><img src="https://useproof.s3.amazonaws.com/turbo1/flamelite.svg"></div> <div class="WpBxqu5HonOpqu5HVcYt_0"><div><span class="WpBxtoc3onOptoc3VcYt_0"><span>2157</span> people</span> signed up in the last 7 days
-      </div></div> <div class="WpBx29e1onOp29e1VcYt_0" style="display: none;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path></svg></div></div></div></div></div></div></div> <div><!----> <!----></div></div>
-      ' . "<style>$style</style>";
+	$html = <<<'HTML'
+	<div id="WpBx2DbIonOp2DbIVcYt_0">
+    <div id="WpBx3tRBonOp3tRBVcYt_0" class="">
+        <div>
+            <div style="">
+                <div style="">
+                    <div id="WpBx2fZIonOp2fZIVcYt_0">
+                        <div class="WpBx3zjZonOp3zjZVcYt_0 yp_onscreen">
+                            <div class="WpBx1JYFonOp1JYFVcYt_0"><img class=""
+                                                                     src="https://firebasestorage.googleapis.com/v0/b/proof-f6589.appspot.com/o/maps%2Fc2ca8c9de6e93ee0d2348d586f1df432.png.png?alt=media">
+                            </div>
+                            <div class="WpBx2EbConOp2EbCVcYt_0">
+                                <div class="WpBx2AUlonOp2AUlVcYt_0">Arun from New Delhi, DL</div>
+                                <div class="WpBx13BWonOp13BWVcYt_0">Recently signed up for Proof</div>
+                                <div class="WpBx2PlWonOp2PlWVcYt_0">2 hours ago</div>
+                                <div class="WpBx3eNuonOp3eNuVcYt_0"></div>
+                            </div>
+                            <div class="WpBx1N_EonOp1N_EVcYt_0" style="display: none;">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style="display: none;">
+                    <div id="WpBx3dGionOp3dGiVcYt_0">
+                        <div class="WpBx2B_4onOp2B_4VcYt_0">
+                            <div id="WpBx3acHonOp3acHVcYt_0">
+                                <div class="WpBxH0WDonOpH0WDVcYt_0">
+                                    <div class="WpBxh0ePonOph0ePVcYt_0"></div>
+                                </div>
+                                <div class="WpBx15RvonOp15RvVcYt_0">
+                                    <div class="WpBx2fwXonOp2fwXVcYt_0"><span
+                                            class="WpBx1Jr6onOp1Jr6VcYt_0"><span>113</span></span> people
+                                    </div>
+                                    are viewing this site
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div></div>
+</div>
+      
+HTML;
+	echo $html . "<style>$style</style>";
 }
