74d776a23b0e9f39543c889ab63c1cc0cf185913
haonx <qtvhao@gmail.com>
2018-03-11 11:05:05 +0700

    init plugin

---
+++ social-proof-toaster.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * Plugin Name: Social Proof Toaster
+ * Plugin URI: https://github.com/qtvhao/social-proof-toaster
+ * Author: Hao Nghiem
+ * Version: 1.0.0
+ * Author URI: http://haonx.taomienphi.website
+ * Text Domain: sproof
+ * License: GPLv3
+ */
+
+/*
+* Init the plugin
+*/
