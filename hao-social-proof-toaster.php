c25eaabdd5782bf494ca7396af7d3563c0cb3643
haonx <qtvhao@gmail.com>
2018-04-16 00:10:59 +0700

    refactoring

--- hao-social-proof-toaster.php
+++ hao-social-proof-toaster.php
@@ -197,30 +197,30 @@ function sproof_inject_footer_start_html() {
 }
 CSS;
 	$toasts = <<<HTML
-	
-            <div style="">
-                <div style="">
-                    <div id="haosf_toast_wrapper_1997">
-                        <div class="haosf_toast_container haosf_slideInUp">
-                            <div class="haosf_toast_container_side_image">
-                            	<img class="" src="$imageSource">
-                            </div>
-                            <div class="haosf_toast_wrapper_heading">
-	                            <div class="haosf_toast_container_heading">
-	                                <div class="haosf_toast_person"><span class="haosf_toast_person_name">Arun</span> from <span class="haosf_toast_person_address">New Delhi, DL</span></div>
-	                                <div class="haosf_toast_message">Recently signed up for Proof</div>
-	                                <div class="haosf_toast_time_diff">2 hours ago</div>
-	                            </div>
-							</div>
-                            <div class="haosf_toast_button_close_wrapper">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path>
-                                </svg>
-                            </div>
-                        </div>
+<div style="">
+    <div style="">
+        <div id="haosf_toast_wrapper_1997">
+            <div class="haosf_toast_container haosf_slideInUp">
+                <div class="haosf_toast_container_side_image">
+                    <img class="" src="$imageSource">
+                </div>
+                <div class="haosf_toast_wrapper_heading">
+                    <div class="haosf_toast_container_heading">
+                        <div class="haosf_toast_person"><span class="haosf_toast_person_name">Arun</span> from <span
+                                class="haosf_toast_person_address">New Delhi, DL</span></div>
+                        <div class="haosf_toast_message">Recently signed up for Proof</div>
+                        <div class="haosf_toast_time_diff">2 hours ago</div>
                     </div>
                 </div>
+                <div class="haosf_toast_button_close_wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path>
+                    </svg>
+                </div>
             </div>
+        </div>
+    </div>
+</div>
 HTML;
 	$html = <<<HTML
 	<div id="haosf_toasts_wrapper">
