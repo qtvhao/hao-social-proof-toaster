78700952c4bd68a65036d1b219c7a6b40ed82a7e
haonx <qtvhao@gmail.com>
2018-04-16 03:08:33 +0700

    use code convention

--- includes/class-toast.php
+++ includes/class-toast.php
@@ -8,8 +8,9 @@ class Toast{
 	}
 
 	private function populate() {
-		$imageSource = $this->getImage();
-		$this->html = <<<HTML
+		$imageSource = $this->get_image();
+		$closeImage  = $this->get_close_image();
+		$this->html  = <<<HTML
 <div class="haosf_toast_container_side_image">
     <img class="" src="$imageSource">
 </div>
@@ -22,14 +23,12 @@ class Toast{
     </div>
 </div>
 <div class="haosf_toast_button_close_wrapper">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path>
-    </svg>
+    $closeImage
 </div>
 HTML;
 	}
 
-	private function getImage() {
+	private function get_image() {
 		$set          = [ '', 'set2', 'set3', 'set4' ][ rand( 1, 4 ) ];
 		$randomSlug   = rand( 1, 100 );
 		$imageSource  = "https://robohash.org/$randomSlug.png?size=100x100&set=$set";
@@ -40,12 +39,20 @@ HTML;
 	/**
 	 * @return mixed
 	 */
-	public function getHtml() {
+	public function get_html() {
 		return $this->html;
 	}
 
 	public function __toString() {
-		return $this->getHtml();
+		return $this->get_html();
+	}
+
+	private function get_close_image() {
+		return <<<HTML
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path>
+    </svg>
+HTML;
 	}
 
 }
