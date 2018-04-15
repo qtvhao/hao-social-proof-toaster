bda445f78a06cf916ee494698962968df02859c8
haonx <qtvhao@gmail.com>
2018-04-16 03:10:50 +0700

    refactoring

--- includes/class-toast.php
+++ includes/class-toast.php
@@ -8,22 +8,20 @@ class Toast{
 	}
 
 	private function populate() {
-		$imageSource = $this->get_image();
-		$closeImage  = $this->get_close_image();
+		$image_source = $this->get_image();
+		$close_image  = $this->get_close_image();
+		$heading     = $this->get_heading();
 		$this->html  = <<<HTML
 <div class="haosf_toast_container_side_image">
-    <img class="" src="$imageSource">
+    <img class="" src="$image_source">
 </div>
 <div class="haosf_toast_wrapper_heading">
     <div class="haosf_toast_container_heading">
-        <div class="haosf_toast_person"><span class="haosf_toast_person_name">Arun</span> from <span
-                class="haosf_toast_person_address">New Delhi, DL</span></div>
-        <div class="haosf_toast_message">Recently signed up for Proof</div>
-        <div class="haosf_toast_time_diff">2 hours ago</div>
+        $heading
     </div>
 </div>
 <div class="haosf_toast_button_close_wrapper">
-    $closeImage
+    $close_image
 </div>
 HTML;
 	}
@@ -52,6 +50,15 @@ HTML;
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.597 17.954l-4.591-4.55-4.555 4.596-1.405-1.405 4.547-4.592-4.593-4.552 1.405-1.405 4.588 4.543 4.545-4.589 1.416 1.403-4.546 4.587 4.592 4.548-1.403 1.416z"></path>
     </svg>
+HTML;
+	}
+
+	private function get_heading() {
+		return <<<HTML
+		<div class="haosf_toast_person"><span class="haosf_toast_person_name">Arun</span> from <span
+                class="haosf_toast_person_address">New Delhi, DL</span></div>
+        <div class="haosf_toast_message">Recently signed up for Proof</div>
+        <div class="haosf_toast_time_diff">2 hours ago</div>
 HTML;
 	}
 
