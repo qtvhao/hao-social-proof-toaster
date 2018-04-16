dce607936237c1e441c0dfc046038725dfd7cd29
haonx <qtvhao@gmail.com>
2018-04-16 19:26:15 +0700

    implements page 'settings'

---
+++ includes/pages/settings.php
@@ -0,0 +1,32 @@
+<div class="wrap about-wrap full-width-layout">
+    <h1>
+		<?php echo __( 'Welcome to Vietnam Social Proof Toaster', 'haosf' ) ?>
+    </h1>
+    <p class="about-text">
+		<?php echo __( 'Thank you for use our plugin. Next, in this page you can get some helpful tips.', 'haosf' ) ?>
+    </p>
+    <h2 class="nav-tab-wrapper wp-clearfix">
+        <a href="<?php echo admin_url( 'options-general.php?page=haosf-proof-toaster' ); ?>"
+           class="nav-tab nav-tab-active">About</a>
+    </h2>
+    <div class="feature-section">
+        <h2>
+			<?php echo __( 'List of awesome Features', 'haosf' ); ?>
+        </h2>
+        <div class="inline-svg full-width">
+            <h3>
+				<?php echo __( 'Pop up notification with total sales at bottom corner when visitor visits product page.',
+					'haosf' ) ?>
+            </h3>
+            <picture>
+                <img src="<?php echo HAOSF_ASSETS_URL . 'images/screenshot-1.png' ?>" alt="" style="width:unset">
+            </picture>
+        </div>
+        <p>
+			<?php echo __( 'We continuously develop this plugin. If you meet any issues or you want to suggest new feature. Do not hesitate to send your request to the developer via below link',
+				'haosf' ) ?>
+            <a href="https://github.com/qtvhao/hao-social-proof-toaster/issues">https://github.com/qtvhao/hao-social-proof-toaster/issues</a>
+        </p>
+
+    </div>
+</div>
