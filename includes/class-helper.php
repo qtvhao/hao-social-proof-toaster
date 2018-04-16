5daa5a98071bfde1edd88765cb32748d95481c91
haonx <qtvhao@gmail.com>
2018-04-16 17:38:55 +0700

    refactoring

--- includes/class-helper.php
+++ includes/class-helper.php
@@ -13,6 +13,39 @@ use WC_Order;
 use WC_Order_Item_Product;
 
 class Helper {
+
+	/**
+	 * Thanks to https://vi.wordpress.org/plugins/woo-total-sales/
+	 *
+	 * @param string $product_id
+	 *
+	 * @return array|null|object
+	 */
+	public static function get_total_sales_per_product( $product_id = '' ) {
+		global $wpdb;
+		$post_status = array( 'wc-completed', 'wc-processing', 'wc-on-hold' );
+		$order_items = $wpdb->get_row( $wpdb->prepare( " SELECT SUM( order_item_meta.meta_value ) as _qty, SUM( order_item_meta_3.meta_value ) as _line_total FROM {$wpdb->prefix}woocommerce_order_items as order_items
+
+			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta ON order_items.order_item_id = order_item_meta.order_item_id
+			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta_2 ON order_items.order_item_id = order_item_meta_2.order_item_id
+			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta_3 ON order_items.order_item_id = order_item_meta_3.order_item_id
+			LEFT JOIN {$wpdb->posts} AS posts ON order_items.order_id = posts.ID
+
+			WHERE posts.post_type = 'shop_order'			
+			AND posts.post_status IN ( '" . implode( "','", $post_status ) . "' )
+			AND order_items.order_item_type = 'line_item'
+			AND order_item_meta.meta_key = '_qty'
+			AND order_item_meta_2.meta_key = '_product_id'
+			AND order_item_meta_2.meta_value = %s
+			AND order_item_meta_3.meta_key = '_line_total'
+
+			GROUP BY order_item_meta_2.meta_value
+
+			", $product_id ) );
+
+		return $order_items;
+	}
+
 	/**
 	 * @param $product_id
 	 *
--- includes/class-order-count-product-social-proof-toast.php
+++ includes/class-order-count-product-social-proof-toast.php
@@ -38,40 +38,8 @@ class Order_Count_Product_Social_Proof_Toast extends Product_Social_Proof_Toast 
 		return $product_name;
 	}
 
-	/**
-	 * Thanks to https://vi.wordpress.org/plugins/woo-total-sales/
-	 *
-	 * @param string $product_id
-	 *
-	 * @return array|null|object
-	 */
-	public static function get_total_sales_per_product( $product_id = '' ) {
-		global $wpdb;
-		$post_status = array( 'wc-completed', 'wc-processing', 'wc-on-hold' );
-		$order_items = $wpdb->get_row( $wpdb->prepare( " SELECT SUM( order_item_meta.meta_value ) as _qty, SUM( order_item_meta_3.meta_value ) as _line_total FROM {$wpdb->prefix}woocommerce_order_items as order_items
-
-			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta ON order_items.order_item_id = order_item_meta.order_item_id
-			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta_2 ON order_items.order_item_id = order_item_meta_2.order_item_id
-			LEFT JOIN {$wpdb->prefix}woocommerce_order_itemmeta as order_item_meta_3 ON order_items.order_item_id = order_item_meta_3.order_item_id
-			LEFT JOIN {$wpdb->posts} AS posts ON order_items.order_id = posts.ID
-
-			WHERE posts.post_type = 'shop_order'			
-			AND posts.post_status IN ( '" . implode( "','", $post_status ) . "' )
-			AND order_items.order_item_type = 'line_item'
-			AND order_item_meta.meta_key = '_qty'
-			AND order_item_meta_2.meta_key = '_product_id'
-			AND order_item_meta_2.meta_value = %s
-			AND order_item_meta_3.meta_key = '_line_total'
-
-			GROUP BY order_item_meta_2.meta_value
-
-			", $product_id ) );
-
-		return $order_items;
-	}
-
 	protected function get_message_bottom() {
-		$total_sales = (int) static::get_total_sales_per_product( $this->get_product()->get_id() )->_qty;
+		$total_sales = (int) Helper::get_total_sales_per_product( $this->get_product()->get_id() )->_qty;
 
 		return sprintf( __( 'Units Sold: %s', 'haosf' ), $total_sales );
 	}
