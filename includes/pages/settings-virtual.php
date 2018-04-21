465dbfcef5f98d22a052077b91d0cd72d84027ac
haonx <qtvhao@gmail.com>
2018-04-21 14:22:15 +0700

    adding cap for custom 3 lines

--- includes/pages/settings-virtual.php
+++ includes/pages/settings-virtual.php
@@ -4,7 +4,7 @@ echo admin_url('options.php') ?>" method="post">
 	<input type="hidden" name="action" value="update">
 	<?php wp_nonce_field('options-options') ?>
 	<input type="hidden" name="option_page" value="options" />
-	<input type="hidden" name="page_options" value="haosf_toast_enable_virtual_product_order_toast,haosf_toast_virtual_product_message_top_format,haosf_toast_product_virtual_social_proof_middle_message,haosf_toast_product_virtual_social_proof_bottom_message" />
+	<input type="hidden" name="page_options" value="haosf_toast_enable_virtual_product_order_toast,haosf_toast_virtual_product_message_top_format,haosf_toast_product_virtual_social_proof_middle_message,haosf_toast_product_virtual_social_proof_bottom_message,haosf_toast_names_repository,haosf_toast_addresses_repository,haosf_toast_product_virtual_total_sales,haosf_toast_product_virtual_subject_format" />
 	<table class="form-table">
 		<tbody>
         <tr valign="top" class="">
@@ -30,10 +30,8 @@ echo admin_url('options.php') ?>" method="post">
             <td class="forminp forminp-checkbox">
                 <fieldset>
                     <label>
-                        <input type="text" class="" title=""
-                               value="<?php
-				               echo esc_attr(Plugin::instance()->setting( 'virtual_product_message_top_format', __('<span class="haosf_toast_person_name">[haosf_random_name]</span> from <span class="haosf_toast_person_address">[haosf_random_address]</span>', 'haosf')));
-				               ?>" name="haosf_toast_virtual_product_message_top_format">
+                        <textarea style="min-width:500px;" rows="3" type="text" class="" title=""
+                                  name="haosf_toast_virtual_product_message_top_format"><?php echo esc_attr(Plugin::instance()->setting( 'virtual_product_message_top_format', __('<span class="haosf_toast_person_name">[haosf_random_name]</span> from <span class="haosf_toast_person_address">[haosf_random_address]</span>', 'haosf'))); ?></textarea>
                     </label>
                 </fieldset>
             </td>
@@ -45,11 +43,9 @@ echo admin_url('options.php') ?>" method="post">
             <td class="forminp forminp-checkbox">
                 <fieldset>
                     <label>
-                        <input type="text" class="" title=""
-                               value="<?php
-				               echo esc_attr( Plugin::instance()->setting( 'product_virtual_social_proof_middle_message',
-					               __( "Just bought <span>%s</span>", 'haosf' ) ));
-				               ?>" name="haosf_toast_product_virtual_social_proof_middle_message">
+                        <textarea style="min-width:500px;" rows="3" type="text" class="" title=""
+                                  name="haosf_toast_product_virtual_social_proof_middle_message"><?php echo esc_attr( Plugin::instance()->setting( 'product_virtual_social_proof_middle_message',
+		                        __( "Just bought <span>%s</span>", 'haosf' ) )); ?></textarea>
                     </label>
                 </fieldset>
             </td>
@@ -61,11 +57,69 @@ echo admin_url('options.php') ?>" method="post">
             <td class="forminp forminp-checkbox">
                 <fieldset>
                     <label>
-                        <input type="text" class="" title=""
-                               value="<?php
-				               echo esc_attr( Plugin::instance()->setting( 'product_virtual_social_proof_bottom_message',
-					               __( "Total sales: %s", 'haosf' ) ));
-				               ?>" name="haosf_toast_product_virtual_social_proof_bottom_message">
+                        <textarea style="min-width:500px;" rows="3" type="text" class="" title=""
+                                  name="haosf_toast_product_virtual_social_proof_bottom_message"><?php echo esc_attr( Plugin::instance()->setting( 'product_virtual_social_proof_bottom_message',
+		                        __( "Total sales: %s", 'haosf' ) )); ?></textarea>
+                    </label>
+                </fieldset>
+            </td>
+        </tr>
+        <tr valign="top" class="">
+            <th scope="row" class="titledesc">
+		        <?php echo __('Names repository for pick randomly','haosf') ?>
+            </th>
+            <td class="forminp forminp-checkbox">
+                <fieldset>
+                    <label>
+                        <textarea type="text" class="" rows="10" style="width:100%;    min-width: 500px;" title="" name="haosf_toast_names_repository"><?php
+	                        $names = Plugin::instance()->setting( 'names_repository',
+		                        require_once HAOSF_PATH . 'includes/names-repository.php' );
+	                        echo is_string($names)?$names:implode( PHP_EOL, $names);
+                            ?></textarea>
+                    </label>
+                </fieldset>
+            </td>
+        </tr>
+        <tr valign="top" class="">
+            <th scope="row" class="titledesc">
+		        <?php echo __('Addresses repository for pick randomly','haosf') ?>
+            </th>
+            <td class="forminp forminp-checkbox">
+                <fieldset>
+                    <label>
+                        <textarea type="text" class="" rows="10" style="width:100%;    min-width: 500px;" title="" name="haosf_toast_addresses_repository"><?php
+	                        $addresses = Plugin::instance()->setting( 'addresses_repository',
+		                        require_once HAOSF_PATH . 'includes/address-repository.php' );
+	                        echo is_string($addresses)?$addresses:implode( PHP_EOL, $addresses);
+                            ?></textarea>
+                    </label>
+                </fieldset>
+            </td>
+        </tr>
+        <tr valign="top" class="">
+            <th scope="row" class="titledesc">
+		        <?php echo __('Edit what show as total sales in bottom line','haosf') ?>
+            </th>
+            <td class="forminp forminp-checkbox">
+                <fieldset>
+                    <label>
+                        <textarea type="text" class="" rows="3" style="width:100%;    min-width: 500px;" title="" name="haosf_toast_product_virtual_total_sales"><?php
+	                        echo Plugin::instance()->setting( 'product_virtual_total_sales', '[haosf_random range="1000,2000" session_remember=1 session_remember_key="uri" session_remember_salt=0]' );
+                            ?></textarea>
+                    </label>
+                </fieldset>
+            </td>
+        </tr>
+        <tr valign="top" class="">
+            <th scope="row" class="titledesc">
+		        <?php echo __('Custom what shown in middle line. Eg. current products or upsell products','haosf') ?>
+            </th>
+            <td class="forminp forminp-checkbox">
+                <fieldset>
+                    <label>
+                        <textarea type="text" class="" rows="3" style="width:100%;    min-width: 500px;" title="" name="haosf_toast_product_virtual_subject_format"><?php
+	                        echo Plugin::instance()->setting('product_virtual_subject_format', "[haosf_product current=1]");
+                            ?></textarea>
                     </label>
                 </fieldset>
             </td>
--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -4,6 +4,7 @@ use Haosf_Social_Proof_Toaster\Plugin;
 
 add_shortcode( 'haosf_random_address', function ($attrs = []) {
 	$addresses_repository = Plugin::instance()->setting( 'addresses_repository', require_once 'address-repository.php');
+	$addresses_repository = (is_string( $addresses_repository)?explode( PHP_EOL, $addresses_repository):$addresses_repository);
 	shuffle( $addresses_repository);
 	return Plugin::instance()->session->remember( 'uri', 'random_address', function () use ( $addresses_repository ) {
 		return $addresses_repository[0];
@@ -11,6 +12,7 @@ add_shortcode( 'haosf_random_address', function ($attrs = []) {
 });
 add_shortcode( 'haosf_random_name', function ($attrs = []) {
 	$names_repository = Plugin::instance()->setting( 'names_repository', require 'names-repository.php' );
+	$names_repository = (is_string( $names_repository)?explode( PHP_EOL, $names_repository):$names_repository);
 	shuffle( $names_repository);
 	return Plugin::instance()->session->remember( 'uri', 'random_name', function () use ( $names_repository ) {
 		return $names_repository[0];
@@ -31,6 +33,7 @@ add_shortcode( 'haosf_random', function ($attrs = []) {
 		return rand($min, $max);
 	}
 	$session_remember_salt = isset($attrs['session_remember_salt'])?$attrs['session_remember_salt']:'number_0';
+	$session_remember_salt.=json_encode( $attrs);
 
 	return Plugin::instance()->session->remember($attrs['session_remember_key'], $session_remember_salt, function() use ( $max, $min ) {
 		return rand($min, $max);
